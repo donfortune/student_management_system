89e65b93404296bb7623586c3eb73c86f248616c
donfortune <donfortunet.df@gmail.com>
2023-06-15 10:54:55 +0100

    configured insert dialog page

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from datetime import datetime
 
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QComboBox, QMainWindow, QTableWidget,  QTableWidgetItem
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QComboBox, QMainWindow, QTableWidget,  QTableWidgetItem, QDialog
 from PyQt6.QtGui import QAction
 import sqlite3
 
@@ -16,6 +16,7 @@ class MainWindow(QMainWindow):    #QMAINWINDOW ALLOWS FOR A MENU AND STATUS BAR
 
         #create sub menu bar
         add_student = QAction('Add Student', self)
+        add_student.triggered.connect(self.insert)
         file_menu.addAction(add_student)
 
         about_us = QAction('About', self)
@@ -40,6 +41,55 @@ class MainWindow(QMainWindow):    #QMAINWINDOW ALLOWS FOR A MENU AND STATUS BAR
             for column, data_ in enumerate(data):
                 self.table.setItem(row, column, QTableWidgetItem(str(data_)))
         connection.close()
+
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Insert Student Data')
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        self.name = QLineEdit()
+        self.name.setPlaceholderText('Name')
+        layout.addWidget(self.name)
+
+        self.courses = QComboBox()
+        self.courses.addItems(['Biology', 'Maths', 'Chemistry', 'Physics'])
+        layout.addWidget(self.courses)
+
+        self.phone_name = QLineEdit()
+        self.phone_name.setPlaceholderText('Phone No.')
+        layout.addWidget(self.phone_name)
+
+        button = QPushButton('Submit')
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+
+    def add_student(self):
+        name = self.name.text()
+        course = self.courses.itemText(self.courses.currentIndex())
+        mobile = self.phone_name.text()
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)', (name, course, mobile))
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+        main_window.add_data()
+
+
+
 
 
 
